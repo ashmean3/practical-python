97affcc17d331d48255b6a5e8e650c21f3c2d714
ashmean3 <ashmeansran3@gmail.com>
2020-07-14 23:28:17 -0230

    Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost= 0.0
+
+
+a = open('Data/portfolio.csv', 'rt')
+headers = next(a)
+for line in a:
+    row= line.split(',')
+    shares= int(row[1])
+    price = float(row[2])
+    total_cost += shares * price
+
+print(f'Total cost {total_cost:0.2f}')
+
+a.close()
