2d16f46f962d60d7c00308e26bd3988a09ff4f0b
ashmean3 <ashmeansran3@gmail.com>
2020-07-19 01:18:24 -0230

    Exercise 2.8 to 2.12

--- report.py
+++ report.py
@@ -11,10 +11,11 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = {'name':(row[0]), 
-                        'shares':int(row[1]), 
-                        'price':float(row[2])
-                      }
+            holding = {
+                    'name':(row[0]), 
+                    'shares':int(row[1]), 
+                    'price':float(row[2])
+            }
             portfolio.append(holding)    
     return portfolio
 
@@ -46,3 +47,27 @@ for s in portfolio:
 print ('Total cost:', total_cost)
 print ('Current Value:', current_value)
 print (f'Gain/Loss: {(current_value-total_cost):.2f}')
+
+# Exercise 2.9
+
+def make_report(portfolio, prices):
+
+    ''' Make a list of tuples with stocks and prices dictionary '''
+
+    rows = []
+    for s in portfolio:
+        current_price = prices[s['name']]
+        change = current_price - s['price']
+        Stock_data = (s['name'], s['shares'], current_price, change)
+        rows.append(Stock_data)
+    return rows
+
+# Exercise 2.10 to 2.12
+
+report = make_report (portfolio, prices)
+headers = ('Name', 'Shares', 'Price', 'Change')
+
+print('%10s %10s %10s %10s' % headers)
+print(('-' * 10 + ' ') * len(headers))
+for r in report:
+        print('%10s %10d %10.2f %10.2f' % r)
