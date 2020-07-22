4261a32ce0c3b8c9eb5efd6bcaa33d5e33537e7c
ashmean3 <ashmeansran3@gmail.com>
2020-07-21 23:04:29 -0230

    Exxercise 3.6

--- fileparse.py
+++ fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None, types=None):
+def parse_csv(filename, select=None, types=None, has_headers=True):
     '''
     Parse a CSV file into a list of records
     '''
@@ -11,7 +11,9 @@ def parse_csv(filename, select=None, types=None):
         rows = csv.reader(f)
 
         # Read the file headers
-        headers = next(rows)
+        if has_headers:
+            headers = next(rows)
+
         if select:
             indices = [headers.index(colname) for colname in select]
             headers = select
@@ -31,9 +33,10 @@ def parse_csv(filename, select=None, types=None):
             if types:
                 row = [func(val) for func, val in zip(types, row)]
 
-            
-
-            record = dict(zip(headers, row))
+            if has_headers:
+                record= dict(zip(headers, row))
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
@@ -41,4 +44,6 @@ def parse_csv(filename, select=None, types=None):
 records= parse_csv('Data/portfolio.csv', types= [str, int, float])
 print(records)
 records= parse_csv('Data/portfolio.csv', types= [str, int], select = ['name', 'shares'])
+print(records)
+records= parse_csv('Data/prices.csv', types= [str, float], has_headers=False)
 print(records)
